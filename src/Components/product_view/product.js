21b1178f270ee7030fdc14093fcf7eb2976d2b2a
dropcmd <dropcmd@gmail.com>
2020-02-19 09:15:44 +0000

    Initial commit

--- src/Components/product_view/product.js
+++ src/Components/product_view/product.js
@@ -44,7 +44,7 @@ export const Product = () => {
 					padding: 50
 				}} xs={12} md={6} lg={7}>
 
-					<h1>{item.name}</h1>
+					<h1>update {item.name}</h1>
 
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
